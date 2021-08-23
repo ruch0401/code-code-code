af6c36ffa43e8db53f174baecf8e31df1ad94520
ruch0401 <ruch0401@gmail.com>
2021-08-23 12:28:40 -0700

    recursion-5 added fast exponentiation approach as well

--- recursion/src/Exponent.java
+++ recursion/src/Exponent.java
@@ -7,8 +7,13 @@ public class Exponent {
         int number = sc.nextInt();
         int power = sc.nextInt();
 
+        System.out.println("Using Normal Exponentiation");
         long result = exponent(number, power);
         System.out.printf("%d raised to the power of %d is %d", number, power, result);
+
+        System.out.println("\nUsing Fast Exponentiation");
+        long resultFast = exponentFast(number, power);
+        System.out.printf("%d raised to the power of %d is %d", number, power, resultFast);
     }
 
     private static long exponent(int number, int power) {
@@ -22,5 +27,20 @@ public class Exponent {
 
         // lines 19 and 20 can be combined in the following manner
         // return number * exponent(number, power - 1);
+    }
+
+    private static long exponentFast(int number, int power) {
+        if (power == 0)
+            return 1;
+
+        // recursion step -> HEAD recursion approach
+        long small_ans = exponentFast(number, power / 2);
+
+        // work step -> follows recursion step hence HEAD recursion
+        // even power
+        if (power % 2 == 0)
+            return small_ans * small_ans;
+        else
+            return number * small_ans * small_ans;
     }
 }
