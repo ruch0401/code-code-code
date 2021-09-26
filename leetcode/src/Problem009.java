2c2f1f96243383f3e5a75413bf411b04872fa91c
ruch0401 <ruch0401@gmail.com>
2021-09-25 21:08:29 -0700

    Goldman Sachs OA Question 2 - with fix

--- leetcode/src/Problem009.java
+++ leetcode/src/Problem009.java
@@ -18,12 +18,20 @@ public class Problem009 {
     public static int solve(int[] arr, int target) {
         int count = 0;
         Map<Integer, Integer> map = new LinkedHashMap<>();
+        Map<Integer, Integer> freq = new LinkedHashMap<>();
+
+        for (int elem: arr) {
+            freq.put(elem, freq.getOrDefault(elem, 0) + 1);
+        }
 
         for (int j : arr) {
             int complement = target - j;
             if (complement >= 0) {
-                if (map.containsKey(complement)) {
+                if (map.containsKey(complement) && freq.get(complement) > 0) {
                     count++;
+                    freq.put(j, freq.get(j) - 1);
+                    if (freq.containsKey(complement))
+                        freq.put(complement, freq.get(complement) - 1);
                 }
                 map.put(j, complement);
             }
