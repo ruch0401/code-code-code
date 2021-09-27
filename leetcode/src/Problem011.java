ef27301a7fd89b7e9bc433d93302088e0254eeab
ruch0401 <ruch0401@gmail.com>
2021-09-27 13:24:48 -0700

    Goldman Sachs OA Longest K-Interspaced Substring

---
+++ leetcode/src/Problem011.java
@@ -0,0 +1,39 @@
+public class Problem011 {
+    public static void main(String[] args) {
+        System.out.println(solve("wedding",0));
+        System.out.println(solve("ababbacaabbbb",1));
+        System.out.println(solve("apple",0));
+        System.out.println(solve("ababbabcbbbbb", 1));
+        System.out.println(solve("aaa", 0));
+        System.out.println(solve("jgmskowvdwpedytaqzfcxlrsasmpnxhbvgbbblvnjltdqrstnltskgiumuxmdzzskblxymrexenfducxqxetphdilrlzvuphlbcnhyczqjbuhfzylngvbxsbtncklltykqcfwvvuhkpkkfpfdfvfzueplbojsfglpkdaadaaelqaobyipldsqxtstelkisrbntgttjmbhzogzpysuosrkzknbtdgnscmmdkrxakczneqwzdaxvkbhpftckkhgbmyejyxvnyrsghdzvoncyiaipitvtrohyhunjyiqiknqvzvmcolfsjyxsqeumkqpwpjhccqicdnrdbmsdvwtdtvebpejetrmjgmhiwgxfmsyinbwceuewizfdcqezgqjycsldimmlvlpiieuzfcrxwwjbmlhbpvdaitqnpjbjuwnjkxwgprtuavpuhemurhsjbjwipfdchcqxvjpylfswjdcnkbxtyvhvdpkkqwjzjbusjlbrxylhtezowycbjpgwlywarxibupcrpcvkpgnfirwiygnmmxyvweesauirxvatypvoakdenlvtvukjssbfhmopconqjcbamnwanqrdfjxlafnbvuctwjkycitmfaephbtidaailqpzdrljnbthsjyemvzyfnlegusbvkmtjimmxmbaxsxsobrsiahisvpvywlpjmynnczojxfncsvujdxcvzcedtwzvxgmxmewicudmehcvcwmdnwxgtmjwujfmfczsmgyzleohsteiiblqjqxulbihxaflbfssuumszsvcrspolpocpvprikjzcwggkxsncyzyvlijgypofnychidhxxgvlbpymfzbmnesdhsuxiixdlrdqpvjicpsdzdzlrqntxgtfhawwgxqlzytrwezjknsmltphlqxsieydaowwlnxatyycswendwxqftxmabrhlhevmrqxgfuhgbpuvhzgwleymdpkfukxbndvswoqienufrirewilkrlsvarmcufbfhesiddftqwmktfmqkxeqzuaxwsuowngxoqmaagxpdvenqftlrfjooudtubjmlclarcbwkckmibeunujkfmzaskybsebkdmqpttanlppbhrkoulnsobrknyaourpkogeiwkocriibbjxzaexvgqrtewvznthtkqunbirofzjcejktqcsxiajiojucjyownbpsbskeuocgtcgvkrlyavaqfbbfqjgwgvhabltmmlmiefmftmqcrtrhojimoqmpaphmwoaxgxjosoqgtzfgwzcrxhekudmgkuyrwitdldopqeyzvftswixwvuxumjugkdjvxlzqzkxejkjiuctnxxtcgezbwntamziadpjereuuvcucufafikosntwxmjqotwbmeuhjnpkqxfmsslqyvtittypbxjpioklsagyjpsinkqfqhyiincgtnctapjojixeegpetisvrblisefjacoaugfxsjyoayhwbzirytfucezukhyciecjysunpzcpnobhqrcpsouecbwfhjzsjastrupyfzcmfqpshrgadafktcugkrgzmddpzkbjhoquzctsmqnjkkegjaxguxivqcoanpqcosrbflrhvxjgclytzucmnzzdqsziutxtkibxfqupqsziedtbccblqqhyhkyoaswoqviwjgotiogmnyjbpalmeiuijjfkkblzhqkyjoxsvrvjfkgbwasgmgedkaurkepgiyzpqjznkllehlcsropxjlherznfrylnsnxnkqqbquphkqarqzmujyyfrawgajfiqspfpncseggbchjoihchtjsctaihbhvdjacjzvjttgiiwoiokgynhusfduaivicxyfpgavyojczxitxaqdnieyulmdphnenycgfpewnahgfxwfduhntxhcmcfhdiqoxpiwumwohtswzcslnmdvmceuiiwjxbjrlkbovagjwvqaotqizvlitinzeeaneshrylxczxvflicyeejzynnvttbgnkuehzaqzouvipqbjcnuszquwezfofwdboucdfxcpjeiggzbfrewgvsnejrqnpmqjpvknrzamdtciaeagjxzjsytuqpnollkxkyvwskaiphlboevozrcavfbjflpfeybucwfnytdheslkkgkorsmaycrmpbtitfcvasjfzurhrhkuoqxveyyxloxjwuxgxvfmdwczocioblfxubuydmckrzebeikvkgkedrlyadfbwzqfhuzgxcqugcueuwvayolauoowlrotidqyvtndhiwvsgmxfypspnvpppuruielcxyxfiksfgkccihfdmhfdahubmutkkbolaqayjddybstrasbypzwjnbuswdoqngumbsmdmemtekrdjexxrvfnliovlmjpblpprdbbfwvokibgrjmfawzlzbdlrqcgxmmemptjpwopjsnlwkdkucrbmrwhbwjjkvewkhilivlvbwvnrldvdhukrvriyenuqwyahvqjmpahmrkkmejmbidoynnrlnamwlwedsvbdcznpgzyesisvfvnhkgvexyipjoxqpuztnqmkyuwrfeprypmuprmufukkstptrzaxbhpfmoljpggnyvagoahsolebqasjssonxwgcttolipgqihspzwrlvwogvhgtqhhjeelltkbvwvkpxodjpppluepepzklsswolbtgwkmuanrawkgwqhibrmptgkqkhvjihgfepdwwhjbqswqxhkianbixgtdyayfkwsqsqsskybfulqphlcorbeatwlmlmyffmdmjtzhnytrisddrlwqpvpmohognlhmedrrffhzzrdrkthiknwjmrrqqhklbtfjhzcktdpktwfornddtljeckcwilnzygdjxdfzbuhgiytnubhpllqllqlieayhtagfdyahaxuuclzvvxlcofzjayhbevhzjonptzoreixqawhcglojrllmuiwziejoxryozwqgwqciwzlbllsexhdnkrqqaasmafscyhstltlamfhokebegjwbfkgmrzukszsitrbwjkzextlngscyexcpfihzklrsgtegdsiojgrzpfahpmrdflcwxejijjzzmmagdzwcxtksydcvbymcavtcisrwggfpovrahuzspmuxlmhdgpbahgfforobvimidueeunljdxgenpqqrnepvogmxotaldefelwlhpawfsgadrdbxqjdrjmyjhxvkjrlquiuymehzjhyqbbdssulwpjhtzyoklavvxmgpfslaktojsxzpchauqppmmzctmngdyvxepsnxsuvdcwnymdqnjnggryikbvswmdjfmscykofmsiqdxijzkmwweqahuexmnogzmxdymldyiadoavhunnztehbgihqlypseglphuaaihamdvnpnkwuykvyohjhndhjortlwafbxatckkfratvtvpfafxxqapfymoqvxmitjsxvirnwxszvoetcevojkuqptebpcpasmbfamreouqfmnpmikkjmrkwkwxqgptzbffnsuzxukdkzuzopvwbzccuopkfwvxayztqohifgrrfejzxtrrrbjxhhoyildtpspasejmhgrwbbqkriqdnrnteaiuefrdkwyylgwramblgdctgwrysashapqjrfmklefmukffitjllgbfsquekqvjgmjdjktwfhuzyclposvohvpnrgsbfdgqalslzhfnjliwgchbqhifotmkhbhbhaqjbvhcvsqxmryngxnwswbsuggjgtdmngetyrohidrmwztoskhfbcdokardebcfguwiyutfnetkwnvwqsvyhudsnhylcolebwjvazsftdvfgpnhmvpgsblwjzdytfauqetynahtiwzdatmvqsxzdgbkbawzmnhguxhrfccpdnkraaycleuldglqgrtewvgupkgxnhsnbhdomsfijaqqookfltvzpondbxwnnjleyrqslvnnxuwwkkuzcteacrbewywpujjakqsqwvlkwhffivbofgwuwxtlibkcb", 5));
+    }
+
+    private static String solve(String s, int k) {
+        StringBuilder temp = new StringBuilder();
+        int len = s.length();
+        String max = "";
+        for (int i = 0; i < len - 1; i++) {
+            temp.append(s.charAt(i));
+            if (gap(s.charAt(i), s.charAt(i + 1)) > k) {
+                max = (temp.length() > max.length()) ? temp.toString(): max;
+                temp = new StringBuilder("");
+            }
+        }
+        if (max.equals(s.substring(0, len))) {
+            max = s;
+        }
+
+        if (max.equals("")) {
+            if (temp.length() == len - 1) {
+                temp.append(s.charAt(len - 1));
+            }
+            return temp.toString();
+        } else {
+            return max;
+        }
+    }
+
+    private static int gap(char a, char b) {
+        return Math.abs(a - b);
+    }
+}
