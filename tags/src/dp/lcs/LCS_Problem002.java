31bb2d491ff9ec3e7a654fc79e891767e2c5b655
ruch0401 <ruch0401@gmail.com>
2021-10-17 17:33:46 -0700

    Dynamic Programming | Longest Common Subsequence | Longest Common Substring

--- tags/src/dp/lcs/LCS_Problem002.java
+++ tags/src/dp/lcs/LCS_Problem002.java
@@ -1,2 +1,40 @@
-package dp.lcs;public class LCS_Problem002 {
+package dp.lcs;
+
+
+public class LCS_Problem002 {
+    public static void main(String[] args) {
+        String[] xs = {"ABCDGH"};
+        String[] ys = {"AEDFHR"};
+
+        for (int i = 0; i < xs.length; i++) {
+            String x = xs[i];
+            String y = ys[i];
+            int p = x.length();
+            int q = y.length();
+            System.out.println(longestCommonSubstring(x, y, p, q));
+        }
+    }
+
+    private static int longestCommonSubstring(String x, String y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        // traverse the entire array again to find the maximum value as the substring can occur at any point in the given string
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : dp) {
+            for (int j = 0; j < dp[0].length; j++) {
+                max = Math.max(max, ints[j]);
+            }
+        }
+        return max;
+    }
 }
---
+++ tags/src/dp/lcs/LCS_Problem003.java
@@ -0,0 +1,2 @@
+package dp.lcs;public class LCS_Problem003 {
+}
