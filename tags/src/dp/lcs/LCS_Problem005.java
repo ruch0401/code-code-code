93d7527b6de308f89b1cef1719506ba97186dc03
ruch0401 <ruch0401@gmail.com>
2021-10-17 21:09:56 -0700

    Dynamic Programming | Longest Common Subsequence | Minimum number of insertions and deletions to transform one string to another

---
+++ tags/src/dp/lcs/LCS_Problem005.java
@@ -0,0 +1,48 @@
+package dp.lcs;
+
+
+/*
+ * Minimum number of deletions and insertions to transform one string into another
+ * Given two strings ‘str1’ and ‘str2’ of size m and n respectively. The task is to remove/delete and insert minimum number of characters from/in str1 so as to transform it into str2. It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some another point.
+ * Example:
+ * Input : str1 = "geeksforgeeks", str2 = "geeks"
+ * Output : Minimum Deletion = 8
+ *          Minimum Insertion = 0
+ *
+ * PROBLEM STATEMENT LINK:
+ */
+public class LCS_Problem005 {
+    public static void main(String[] args) {
+        String[] xs = {"ABCDGH", "abcdaf", "geek"};
+        String[] ys = {"AEDFHR", "abcf", "eke"};
+
+        for (int i = 0; i < xs.length; i++) {
+            String x = xs[i];
+            String y = ys[i];
+            int p = x.length();
+            int q = y.length();
+            numberOfInsertionsAndDeletions(x, y, p, q);
+        }
+    }
+
+    private static void numberOfInsertionsAndDeletions(String x, String y, int m, int n) {
+        int l = lcs(x, y, m, n);
+        System.out.println("Insertions: " + (n - l));
+        System.out.println("Deletions: " + (m - l));
+    }
+
+    private static int lcs(String x, String y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
