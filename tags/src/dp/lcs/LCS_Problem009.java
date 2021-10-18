f2104be7b8f5f82de970fa27be2bb5c818c24471
ruch0401 <ruch0401@gmail.com>
2021-10-18 15:41:54 -0700

    Dynamic Programming | Longest Common Subsequence | Sequence Pattern Matching

--- tags/src/dp/lcs/LCS_Problem009.java
+++ tags/src/dp/lcs/LCS_Problem009.java
@@ -1,5 +1,14 @@
 package dp.lcs;
 
+/*
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ *
+ * Example 1:
+ *
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ */
 public class LCS_Problem009 {
     public static void main(String[] args) {
         String[] xs = {"ABCDGH", "AGGTAB", "HELLO", "geek", "aba"};
