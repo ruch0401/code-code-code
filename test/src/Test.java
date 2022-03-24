52b88183e04289ba6b54bef4b98920015bda992e
Ruchit Bhardwaj <ruchitbh@usc.edu>
2022-03-23 18:41:44 -0700

    Java features testing

---
+++ test/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+
+        StringBuilder sb1 = new StringBuilder("Ruchit");
+        StringBuilder sb2 = new StringBuilder("Ruchit");
+        System.out.println(sb1.equals(sb2));
+        System.out.println(sb1 == sb2);
+    }
+}
